c0ddb94abe23dea32e1855714f9132a7473fcf9b
Qchi-07 <chichi07x@outlook.com>
2026-04-30 16:50:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -49,12 +49,10 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_URL1: 'https://www.pan0ku.com/article/top',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL2: 'https://www.pan0ku.com/article/top',
+  HEO_INFO_CARD_TEXT2: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
