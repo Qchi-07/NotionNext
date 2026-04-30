674830a399537bfe9babcfed0faf4441234578ab
Qchi-07 <chichi07x@outlook.com>
2026-04-30 14:34:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,14 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2026-03-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '', url: '' },
-    { title: '建议收藏好本站丨防走失备用网站丨点击跳转', url: 'https://my.feishu.cn/wiki/GoXAwsO2AiKOBOksd0ZcxmeTnUd' },
-    { title: '夸克新用户领1TB云空间', url: 'https://pan.quark.cn/s/bf7238dca17f' },
-    { title: '资料打印丨5分钱丨顺丰到家', url: 'https://my.feishu.cn/wiki/NbXgwUjTriTpczkSZduc2oQCnGd' }
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,23 +18,35 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '专注分享',
-  HEO_HERO_TITLE_2: '全网稀缺实用资源',
+  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '支持一键转存/高速下载',
-  HEO_HERO_TITLE_5: '每天持续更新',
-  HEO_HERO_TITLE_LINK: 'https://www.pan0ku.com/category',
+  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '小编自用', url: '/tag/小编自用' },
-  HEO_HERO_CATEGORY_2: { title: 'AI神器', url: '/tag/AI神器' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+
+  // 右侧个人资料卡牌欢迎语，点击可自动切换
+  HEO_INFOCARD_GREETINGS: [
+    '你好！我是',
+    '🔍 分享与热心帮助',
+    '🤝 专修交互与设计',
+    '🏃 脚踏实地行动派',
+    '🏠 智能家居小能手',
+    '🤖️ 数码科技爱好者',
+    '🧱 团队小组发动机'
+  ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
